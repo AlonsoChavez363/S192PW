1d6d89be1c2c264537e274c5348cc231740c86c8
AlonsoChavez363 <122044170@upq.edu.mx>
2024-10-09 18:58:05 -0600

    avance actividad 4

--- IntroLaravel/routes/web.php
+++ IntroLaravel/routes/web.php
@@ -2,16 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\controladorVistas;
+
+Route::get('/inicio', [controladorVistas::class, 'home'])->name ("inicio");
+Route::get('/form', [controladorVistas::class, 'formulario'])->name ("formulario");
+Route::get('/client', [controladorVistas::class, 'consulta'])->name ("clientes");
+Route::view('component', 'componentes') ->name ('componentes');     
+
+
+
+
+
+
+
+
+
+
+
 //ruta tipo get
 /* Route::get('/', function () {
     return view('welcome');
 }); */
 
 //ruta tipo view
-Route::view('inicio', 'inicio') ->name ("inicio");
+/* Route::view('inicio', 'inicio') ->name ("inicio");
 
 Route::view('form', 'formulario') ->name ('formulario');
 
 Route::view('client', 'clientes') ->name ('clientes');
 
-Route::view('component', 'componentes') ->name ('componentes');
+ */
+
+
